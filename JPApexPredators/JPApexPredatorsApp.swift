85dc26b325172c4c114814f07e509ec5ded6c3a2
Nils Müller <shimst3r@gmail.com>
2024-11-18 17:21:08 +0100

    Initial Commit

---
+++ JPApexPredators/JPApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorsApp.swift
+//  JPApexPredators
+//
+//  Created by Nils Müller on 18.11.24.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JPApexPredatorsTests/JPApexPredatorsTests.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorsTests.swift
+//  JPApexPredatorsTests
+//
+//  Created by Nils Müller on 18.11.24.
+//
+
+import Testing
+@testable import JPApexPredators
+
+struct JPApexPredatorsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
