26ed7bf214c1d63f9f0cf6e3b7cf261eee084b2c
Nils Müller <shimst3r@gmail.com>
2024-11-19 20:30:53 +0100

    feat: add TODOs for challenge

--- JPApexPredators/JPApexPredatorsApp.swift
+++ JPApexPredators/JPApexPredatorsApp.swift
@@ -7,6 +7,11 @@
 
 import SwiftUI
 
+// TODO: 1. Make the predator in the detail view clickable to open a full screen image.
+// TODO: 2. Add an info card to the map screen when clicking on a single predator.
+// TODO: 3. Make list filterable by movies of appearance.
+// TODO: 4. Make a predator deletable from the list.
+
 @main
 struct JPApexPredatorsApp: App {
     var body: some Scene {
